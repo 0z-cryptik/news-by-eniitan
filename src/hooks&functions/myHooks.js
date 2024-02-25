943ac58142bda802b8cea08c897f44c8783a5dc3
Eniitan <enitanolawale0@gmail.com>
2024-02-25 02:15:17 +0100

    progress with code refactor

--- src/hooks&functions/myHooks.js
+++ src/hooks&functions/myHooks.js
@@ -7,7 +7,7 @@ export const useList = () => useContext(ListContext);
 
 export const ListProvider = ({ children }) => {
   const [activeCategory, setActiveCategory] = useState(null);
-  const [dark, setDark] = useLocalStorage("darkMode", false);
+  const [dark, setDark] = useLocalStorage("darkMode", true);
   const [viewMenu, setViewMenu] = useState(false);
 
   return (
@@ -31,14 +31,12 @@ export const useGetNews = (category) => {
       `https://api.nytimes.com/svc/topstories/v2/${category}.json?api-key=${process.env.REACT_APP_API_KEY}`
     );
 
-    const posts = news.data.results;
+    const posts = await news.data.results;
 
-    const filteredList = posts.filter(
-      (item) => item.multimedia && item.multimedia.length
-    );
+    const filteredList = posts.filter((item) => item.multimedia.length);
 
     return filteredList;
   };
 
-  return [getNews]
+  return [getNews];
 };
